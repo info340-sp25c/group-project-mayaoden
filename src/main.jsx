ab8446b6636c5dcb397564ab849cccc564c103d5
Connor Lam <clam92@uw.edu>
2025-05-27 11:40:34 -0700

    set up firebase config

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,28 @@ import './index.css'
 import App from './App.jsx'
 import { StrictMode } from 'react'
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCu-s9BnGxNcMZd5F2DXecJbL4MTQ886nE",
+  authDomain: "group-project-mayaoden.firebaseapp.com",
+  databaseURL: "https://group-project-mayaoden-default-rtdb.firebaseio.com",
+  projectId: "group-project-mayaoden",
+  storageBucket: "group-project-mayaoden.firebasestorage.app",
+  messagingSenderId: "254264394038",
+  appId: "1:254264394038:web:17fa6a88f046bd993bf397",
+  measurementId: "G-CWVM4CFJM9"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getDatabase(app);
+
+export { db };
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
